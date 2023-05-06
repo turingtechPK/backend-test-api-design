94ce76d56d85952cb2bd0c4de843a187ab601a39
zohaibadnan137 <zohaibadnan137@gmail.com>
2023-05-06 12:48:02 +0500

    🐛 fix: Database connection

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
+require("dotenv").config(); // Load environment variables from .env file
 
+// Connect to database
 async function connectToDatabase() {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true, // Use new URL parser instead of deprecated one
       useUnifiedTopology: true, // Use new Server Discover and Monitoring engine instead of deprecated one
     });
