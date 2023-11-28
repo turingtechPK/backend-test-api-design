0b469eb56375e974b4923207a6777eea863cb522
Alishbaiftikhar <alishbaiftikhar2001@gmail.com>
2023-11-29 02:41:18 +0500

    test change

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,29 +1,27 @@
 const request = require('supertest');
-const app = require('../app'); // Adjust the path to your Express app
+const app = require('../app'); 
 const mongoose = require('mongoose');
-const Contributor = require('../models/Contributor'); // Adjust the path to your Mongoose model
+const Contributor = require('../models/Contributor'); 
 
 describe('API Endpoints', () => {
-  // Before all tests run, connect to the database
+  
   beforeAll(async () => {
     await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
   });
 
-  // After all tests run, close the database connection
+  
   afterAll(async () => {
     await mongoose.connection.close();
   });
 
   test('GET /api/org/repo/year/month - fetch new contributors', async () => {
-    // Here you should set up any necessary mock data in the database
+    
+    const response = await request(app).get('/api/facebook/react/2020/06'); 
 
-    // Make a request to your endpoint
-    const response = await request(app).get('/api/facebook/react/2020/06'); // Adjust the endpoint as necessary
-
-    // Assertions
+   
     expect(response.statusCode).toBe(200);
-    // Add more assertions based on your expected response structure
+    
   },1000000);
 
-  // Add more tests for other endpoints
+  
 });
